54c69524ff6235807f08a3a3ff99162d32581a8a
Jermaine <jermaineanderson1995@yahoo.com>
2019-03-15 11:04:31 -0400

    Comment Out Java Test

--- src/test/java/com/revature/testing/UserServiceTest.java
+++ src/test/java/com/revature/testing/UserServiceTest.java
@@ -29,25 +29,25 @@ public class UserServiceTest {
 		testLoginUser = null;
 	}
     
-    /* Wrong login credentials, expected exception */
+    // Wrong login credentials, expected exception 
     @Test(expected = LoginFailedException.class)
 	public void loginTest() throws LoginFailedException {
 		UserService.getUserService().login(testLoginUser);
 	}
     
-    /* Users existing in db, expected list with more than 0 users */
+    // Users existing in db, expected list with more than 0 users 
     @Test
     public void getAllUsersTest() {
     	assertTrue(UserService.getUserService().getAllEmployees().size() > 0);
     }
     
-    /* Username already exists in database, expected true */
+    // Username already exists in database, expected true 
     @Test
 	public void isUsernameTakenTest()  {
 		assertTrue(UserService.getUserService().isUsernameTaken(testUser.getUsername()));		
 	}
    
-    /* Username existing in db, expected same user as returned */
+    // Username existing in db, expected same user as returned 
     @Test
     public void getUserInfoTest() {
     	User user = UserService.getUserService().getUserInfo(testUser.getUsername());
